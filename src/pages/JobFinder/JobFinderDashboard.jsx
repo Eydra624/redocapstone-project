d2024f36d8a5323911042982fd8fd275777db7d6
Eydra624 <eydrachidimma@gmail.com>
2025-10-29 13:05:10 +0100

    CI/CD pipelines

--- src/pages/JobFinder/JobFinderDashboard.jsx
+++ src/pages/JobFinder/JobFinderDashboard.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import jobs from "../../data";
 import JobCard from "../../components/JobCard";
-import "../../css/jobFinderDashboard.css";
+import "../../css/JobFinderDashboard.css";
   const JobFinderDashboard = () => {
   const location = useLocation();
   const [searchTerm, setSearchTerm] = useState("");
